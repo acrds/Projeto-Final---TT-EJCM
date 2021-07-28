973b5cc9adbf116b7c4a40e4cb22e4702744d0b3
Raphael Basto <raphaelpgpb@icloud.com>
2021-07-27 21:37:57 -0300

    feature: Adicionando e configurando rotas das controllers

--- back/src/routes/routes.js
+++ back/src/routes/routes.js
@@ -1,1 +1,32 @@
+const { Router } = require('express');
+const ProductController = require('../controllers/ProductController');
+const CategoryController = require('../controllers/CategoryController');
+const SaleController = require('../controllers/SaleController');
+const ShopListController = require('../controllers/ShopListController');
+const router = Router();
 
+router.get('/products',ProductController.index);
+router.get('/products/:id',ProductController.show);
+router.post('/products',ProductController.create);
+router.put('/products/:id', ProductController.update);
+router.delete('/products/:id', ProductController.destroy)
+
+router.get('/categories',CategoryController.index);
+router.get('/categories/:id',CategoryController.show);
+router.post('/categories',CategoryController.create);
+router.put('/categories/:id', CategoryController.update);
+router.delete('/categories/:id', CategoryController.destroy)
+
+router.get('/sales',SaleController.index);
+router.get('/sales/:id',SaleController.show);
+router.post('/sales',SaleController.create);
+router.put('/sales/:id', SaleController.update);
+router.delete('/sales/:id', SaleController.destroy)
+
+router.get('/shopLists',ShopListController.index);
+router.get('/shopLists/:id',ShopListController.show);
+router.post('/shopLists',ShopListController.create);
+router.put('/shopLists/:id', ShopListController.update);
+router.delete('/shopLists/:id', ShopListController.destroy)
+
+module.exports = router;
