13db8b7d0208fd0549a39f528c6c3661d5f283dc
Gabriela Santos <gbss.santos@poli.ufrj.br>
2021-07-29 11:57:28 -0300

    feature: Tela de cadastro

--- front/App.tsx
+++ front/App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Cadastro from "./src/pages/cadastro";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Cadastro/>
     </View>
   );
 }
@@ -14,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
