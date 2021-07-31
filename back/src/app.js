3859eb66d1a4d15be24c66d1e0e350b84940bd30
Ana Claudia Santos <anasantos.rds@outlook.com>
2021-07-30 21:37:08 -0300

    config: Package, yarn, app e sequelize com novas funcionalidades

--- back/src/app.js
+++ back/src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('./config/dotenv')();
 require('./config/sequelize');
-
+const path = require('path');
 const app = express();
 const port = process.env.PORT;
 //const cors = require('cors');
@@ -16,6 +16,8 @@ app.use(routes);
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
+
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 
 app.listen(port, () => {
   console.log(`${process.env.APP_NAME} app listening at http://localhost:${port}`);
