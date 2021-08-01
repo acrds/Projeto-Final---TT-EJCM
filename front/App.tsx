ac6a27dc80e586acfc110e08639e455f8695367c
Gabriela Santos <gbss.santos@poli.ufrj.br>
2021-08-01 16:13:56 -0300

    feature: Apresentação do pedido

--- front/App.tsx
+++ front/App.tsx
@@ -4,14 +4,14 @@ import { StyleSheet, Text, View } from 'react-native';
 import Cadastro from "./src/pages/cadastro";
 import Home from './src/pages/home';
 import Favoritos from './src/pages/favoritos';
-import Compra from './src/pages/compra';
+import Carrinho from './src/pages/carrinho';
 
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Home/>
+      <Carrinho/>
     </View>
   );
 }
