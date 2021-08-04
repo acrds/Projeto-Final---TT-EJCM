590125afbb2f122e0350552e210db8a889765461
Raphael Basto <raphaelpgpb@icloud.com>
2021-08-04 17:51:38 -0300

    feature: Adicionando seeder de Review

---
+++ back/src/database/seeders/ReviewSeeder.js
@@ -0,0 +1,23 @@
+const Review = require("../../models/Review");
+const faker = require('faker-br');
+
+ const seedReview = async function () {
+   try {
+     await Review.sync({ force: true });
+
+     for (let i = 0; i < 10; i++) {
+
+      let review = await Review.create({
+        comment: faker.lorem.paragraph(),
+        UserId: i+1,
+        ProductId: i+1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      });
+
+    }
+
+  } catch (err) { console.log(err +'!'); }
+}
+
+module.exports = seedReview;
