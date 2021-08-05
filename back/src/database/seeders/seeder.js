e60aa748c3216987a8d95da77cd214bbc5933011
Ana Claudia Santos <anasantos.rds@outlook.com>
2021-08-05 12:16:41 -0300

    feature: Configurando seeder.js

--- back/src/database/seeders/seeder.js
+++ back/src/database/seeders/seeder.js
@@ -1,11 +1,15 @@
 require('../../config/dotenv')();
 require('../../config/sequelize');
 
-//const seedModel = require('./Model');
+const sequelize = require('../../config/sequelize');
+const seedSale = require('./SaleSeeder');
+const seedShoplist = require('./ShopListSeeder');
 
 (async () => {
   try {
-    //await seedModel();
+    await sequelize.sync({ force: true });
+    await seedShoplist();
+    await seedSale();
 
   } catch(err) { console.log(err) }
 })();
